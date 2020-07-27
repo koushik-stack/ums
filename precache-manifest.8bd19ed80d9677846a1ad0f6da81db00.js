e5160f1d8677a3173e43729eae2e61a2a2130ad5
koushik-stack <koushiksk.ks@gmail.com>
2020-07-27 19:24:38 +0600

    Updates

---
+++ precache-manifest.8bd19ed80d9677846a1ad0f6da81db00.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe32bfe04d3379cacdb6c7e5f54c771",
+    "url": "/ums/index.html"
+  },
+  {
+    "revision": "3f26459fcd13ff4066bb",
+    "url": "/ums/static/css/main.2c0d3c60.chunk.css"
+  },
+  {
+    "revision": "a7f568b3cffb5625a27f",
+    "url": "/ums/static/js/2.1c6ae5a7.chunk.js"
+  },
+  {
+    "revision": "0a8da621bfabd2204f333cc7e0deceb7",
+    "url": "/ums/static/js/2.1c6ae5a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f26459fcd13ff4066bb",
+    "url": "/ums/static/js/main.e4f32f47.chunk.js"
+  },
+  {
+    "revision": "6e6ac20c88c343d8ef22",
+    "url": "/ums/static/js/runtime-main.85aa536c.js"
+  },
+  {
+    "revision": "781c3d8036c4ff124f88b50602cd68e1",
+    "url": "/ums/static/media/active.781c3d80.svg"
+  },
+  {
+    "revision": "ccb0e9d99692f28a97faad93b56547b4",
+    "url": "/ums/static/media/assignment.ccb0e9d9.svg"
+  },
+  {
+    "revision": "57775afe1371455cdba339940cd1a23b",
+    "url": "/ums/static/media/attendance.57775afe.svg"
+  },
+  {
+    "revision": "f8609941f03efb7c846d3e4f2557554f",
+    "url": "/ums/static/media/card_attendance.f8609941.svg"
+  },
+  {
+    "revision": "9f7fa32ba99970713d15a48717948a5f",
+    "url": "/ums/static/media/card_classes.9f7fa32b.svg"
+  },
+  {
+    "revision": "02593b3602c4b6d37bd2a82c94550709",
+    "url": "/ums/static/media/card_student.02593b36.svg"
+  },
+  {
+    "revision": "52f13c8bc84d6a9980be8a33bb3a9e79",
+    "url": "/ums/static/media/card_student_1.52f13c8b.svg"
+  },
+  {
+    "revision": "b84806a3ddaecff5d8c4626acbc54cc6",
+    "url": "/ums/static/media/card_student_2.b84806a3.svg"
+  },
+  {
+    "revision": "c21dc5ce9c1abcdda1a914a1d907775c",
+    "url": "/ums/static/media/classes.c21dc5ce.svg"
+  },
+  {
+    "revision": "9e460af2afa258853d0b0a5c520bf753",
+    "url": "/ums/static/media/massege.9e460af2.svg"
+  },
+  {
+    "revision": "8ea47e3537d07ba69dcfb25148d5c377",
+    "url": "/ums/static/media/notification.8ea47e35.svg"
+  },
+  {
+    "revision": "ce4209f3cfceeddbb94c8a65a546fe50",
+    "url": "/ums/static/media/profile_pic.ce4209f3.JPG"
+  },
+  {
+    "revision": "67d15d2c8d4207e3d038d05a13402bbb",
+    "url": "/ums/static/media/s1.67d15d2c.svg"
+  },
+  {
+    "revision": "b9f08b8cb9cc91eaa7f9b6ebb503edec",
+    "url": "/ums/static/media/seu-logo.b9f08b8c.png"
+  }
+]);
